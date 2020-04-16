3cfad28a174d6d24c7cbac504a99e44245b4929e
Esteban Torres <esteban13torres@gmail.com>
2020-04-15 20:39:59 -0600

    Fix en las condiciones

--- Caso4/Caso4/main.cpp
+++ Caso4/Caso4/main.cpp
@@ -16,8 +16,8 @@ using namespace std;
 void leerArchivo()
 {
 	wifstream archivo("Texto.txt");
-	
-	if (archivo.is_open()){
+
+	if (archivo.is_open()) {
 
 		// Dar formato utf-8
 		locale utf8_locale(locale(), new codecvt_utf8<wchar_t>); //TODO da error
@@ -26,66 +26,48 @@ void leerArchivo()
 		wstring word, parrafo;
 		int numeroDeParrafo = 1;
 		wchar_t caracter, bom = L'\0';
-				
+
 		archivo.get(bom);
 
 		while (archivo.get(caracter))
-		{	
+		{
 			if (iswalpha(caracter) || caracter == ' ' || caracter == '\n')
 			{
 				caracter = tolower(caracter); // para evitar aumento de comparaciones en el diccionario
 				if (iswalpha(caracter)) word.push_back(caracter);
 
-				else if (caracter == ' ')
+				else // else if (caracter == " " || caracter == "\n")
 				{
+					if (caracter == '\n') numeroDeParrafo++;
+
 					if (!word.empty())
 					{
 						size_t wordSize = word.length();
-						if ( ! ((filtro.find(word)) != filtro.end())
-
-							||
-								!( (wordSize >1 && (word.substr(wordSize-2, wordSize-1) == L"ar"))
-									  
-								||
-									
-								( (wordSize > 3) && (word.substr(wordSize - 3, wordSize - 1) == L"cho") )
-								
-								||
-								
-								(wordSize > 4) && word.substr(wordSize - 4, wordSize - 1)  == L"ando")
-									
-								||
-
-								((wordSize > 5) && word.substr(wordSize - 5, wordSize - 1)  == L"iendo")						
-							) 
+						if (wordSize > 1)
+						{
+							wstring subWord = word.substr(wordSize - 2, wordSize - 1);
+						}
+						if ((filtro.find(word) == filtro.end()) &&
+							(wordSize > 1 ) && (word.substr(wordSize - 2, wordSize - 1) != L"ar") &&
+							(word.substr(wordSize - 2, wordSize - 1) != L"ir") &&
+							(word.substr(wordSize - 2, wordSize - 1) != L"er") &&
+							(word.substr(wordSize - 2, wordSize - 1) != L"so") &&
+							(word.substr(wordSize - 2, wordSize - 1) != L"to") &&
+							(wordSize > 3) && (word.substr(wordSize - 3, wordSize - 1) != L"cho") &&
+							(wordSize > 4) && (word.substr(wordSize - 4, wordSize - 1) != L"ando") &&
+							(wordSize > 5) && (word.substr(wordSize - 5, wordSize - 1) != L"iendo") )
 						{
 							wcout << word << "\n";  //Realmente acá iría el insertar en grafo usar numeroParrafo  
 						}
 						/*
-						(word.substr(wordSize - 2, wordSize - 1) == L"ir") ||
-							(word.substr(wordSize - 2, wordSize - 1) == L"er") ||
-							(word.substr(wordSize - 2, wordSize - 1) == L"so") ||
-							(word.substr(wordSize - 2, wordSize - 1) == L"to")
-							))
+						
 							*/
 						word.clear();
 					}
-					// Se verifica si word es un sustantivo
-				}
-				else if (caracter == '\n')
-				{
-					if (!word.empty())
-					{
-						//wcout << word << "\n";
-						word.clear();
-					}
-					//Nueva linea equivale a nuevo parrafo en la mayoria de los casos
-					//wcout << "Parrafo #" << numeroDeParrafo << "\n";
-					numeroDeParrafo++;
 				}
 			}
 		}
-		
+
 	}
 	else
 	{
@@ -96,7 +78,7 @@ void leerArchivo()
 int main()
 {
 	int m = _setmode(_fileno(stdout), _O_U16TEXT); // Se cambia la consola a utf-16
-	
+
 	leerArchivo();
 	//system("PAUSE");
 	return 1;
